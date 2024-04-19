3dfad9bd7c504c1a47e18d93a5037a1adcba8709
Charlie <sri.pagala@enine.school>
2024-04-19 15:10:35 +0530

    completed the task

--- assignment005/solutions/task_3.sh
+++ assignment005/solutions/task_3.sh
@@ -5,14 +5,11 @@ read n
 # length of arr = ${#a[@]}
 
 
-factorial=(1 1)
-
-    for (( i=0; i < n; ++ i ))
-    do
-        if [[ n > 1 ]]
-        then
-            fact=$((factorial[i-1]*i))
-            factorial+=($fact)
-        fi
-        echo $i,${factorial[i]}
-    done > result.txt
+fact=1
+i=0
+while (($i <= $n)) 
+do
+    echo $i,$fact
+    ((++ i))
+    fact=$((fact * i))
+done > result.txt
