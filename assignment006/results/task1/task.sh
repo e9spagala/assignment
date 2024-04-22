377b84fe340bb5b98c84c872bf9b156347ed45a8
Charlie <sri.pagala@enine.school>
2024-04-22 14:24:07 +0530

    completed task1

---
+++ assignment006/results/task1/task.sh
@@ -0,0 +1,14 @@
+
+OPFILE="./asia_sale.csv"
+# add the headers to the op file
+echo "Region,Country,Total Revenue,Total Cost,Total Profit" > $OPFILE
+
+# find the files that contains Sales through find command
+sales_files=$(find ../../test_data/ -type f -name "*Sales*")
+
+# for every file that has Sales as a keyword bring 1,2,12,13,14 columns.
+
+for i in $sales_files
+do
+    awk -F, '{print $1,$2,$12,$13,$14}' $i >> $OPFILE 
+done
